8bfcdd2276f5c19196fa55438d16cafa5fbb617a
irinadeeva <deeva97@me.com>
2024-11-20 14:56:32 +0300

    Initial Commit

---
+++ Task 7 StretchyImage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task 7 StretchyImage
+//
+//  Created by Irina Deeva on 20/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
