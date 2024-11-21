d63fb60bc32a3edd40d45d711a2e8ab173bf7f8b
irinadeeva <deeva97@me.com>
2024-11-21 11:50:23 +0300

    init

--- Task 7 StretchyImage/ViewController.swift
+++ Task 7 StretchyImage/ViewController.swift
@@ -7,13 +7,88 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController, UIScrollViewDelegate {
+  let scrollView: UIScrollView = {
+    let scrollView = UIScrollView()
+    scrollView.contentSize = CGSize(width: UIScreen.main.bounds.size.width , height: UIScreen.main.bounds.size.height * 2)
+    scrollView.automaticallyAdjustsScrollIndicatorInsets = false
+    return scrollView
+  }()
+
+  let headerView: UIImageView = {
+    let view = UIImageView()
+    view.image = UIImage(named: "DogImage")
+    view.contentMode = .scaleAspectFill
+    view.clipsToBounds = true
+    return view
+  }()
+
+  var headerViewHeightConstraint: NSLayoutConstraint?
+  var headerViewTopConstraint: NSLayoutConstraint?
+
+  let headerViewMinHeight = 270.0
+  let headerViewMaxHeight = UIScreen.main.bounds.size.height / 2
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+
+    setupView()
   }
 
+  func setupView(){
+    view.addSubview(scrollView)
+    view.addSubview(headerView)
 
+    scrollView.translatesAutoresizingMaskIntoConstraints = false
+    headerView.translatesAutoresizingMaskIntoConstraints = false
+
+    scrollView.delegate = self
+
+    headerViewHeightConstraint = headerView.heightAnchor.constraint(equalToConstant: headerViewMinHeight)
+    headerViewTopConstraint = headerView.topAnchor.constraint(equalTo: view.topAnchor)
+
+    NSLayoutConstraint.activate([
+      headerViewTopConstraint!,
+      headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+      headerView.trailingAnchor.constraint(equalTo:view.trailingAnchor),
+      headerView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+      headerViewHeightConstraint!,
+
+      scrollView.topAnchor.constraint(equalTo: headerView.bottomAnchor),
+      scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+      scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+      scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+    ])
+  }
+
+  func scrollViewDidScroll(_ scrollView: UIScrollView) {
+    let offsetY = scrollView.contentOffset.y
+    let swipingDown = offsetY < 0
+    let swipingUp = offsetY > 0
+
+    let current = headerViewHeightConstraint!.constant
+
+    if swipingDown {
+      headerViewTopConstraint?.constant = offsetY
+      headerViewHeightConstraint?.constant =  min(current - offsetY, headerViewMaxHeight)
+    }
+
+    if swipingUp {
+      headerViewTopConstraint?.constant = -min(offsetY, headerViewMinHeight)
+      headerViewHeightConstraint?.constant = headerViewMinHeight
+    }
+  }
+
+  func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+      let isExpanded = headerViewHeightConstraint!.constant > headerViewMinHeight
+
+      if isExpanded {
+        UIView.animate(withDuration: 1, delay: 0, options: [.curveEaseOut], animations: {
+          self.headerViewHeightConstraint?.constant = self.headerViewMinHeight
+          self.headerViewTopConstraint?.constant = 0
+        }, completion: nil)
+      }
+    }
 }
+
 
